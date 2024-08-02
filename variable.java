1c6cea76067c34e62dca09db018966d19373288e
manish31823 <chaudhary.manish76543@gmail.com>
2024-08-02 20:37:00 +0530

    Variable

---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable {
+    public static void main(String[] args) {
+       int num1 = 5;
+        int num2 = 20;
+        int result = num1+num2;
+
+        System.out.println(result);
+        
+    }
+    
+}
