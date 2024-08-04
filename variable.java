1418e73753848484e6d07719262a024dcb319ba3
manish31823 <chaudhary.manish76543@gmail.com>
2024-08-05 00:01:09 +0530

    add varaible

--- variable.java
+++ variable.java
@@ -1,11 +1,18 @@
-public class variable {
-    public static void main(String[] args) {
-       int num1 = 5;
-        int num2 = 20;
-        int result = num1+num2;
+// public class variable {
+//     public static void main(String[] args) {
+//        int num1 = 5;
+//         int num2 = 20;
+//         int result = num1+num2;
 
-        System.out.println(result);
+//         System.out.println(result);
         
-    }
+//     
     
+
+public class  variable {
+    public static void main(String[] args) {
+    String name = "John";
+System.out.println(name);
+}
+ 
 }
